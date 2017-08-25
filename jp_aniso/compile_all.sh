d11e27236767083ae838f19c155e480c8acc015b
Lili Feng <lili.feng@colorado.edu>
2017-08-24 22:48:15 -0600

    Aug 24th, 2017

---
+++ jp_aniso/compile_all.sh
@@ -0,0 +1,2 @@
+#f2py -h anisojp.pyf -m anisojp forward_solver.f90 refft.c aniprop_subroutines.f  eispack.f  rf_aniso_subroutines.f  util_subroutines.f
+f2py -c anisojp.pyf forward_solver.f90 refft.c aniprop_subroutines.f  eispack.f  rf_aniso_subroutines.f  util_subroutines.f -lfftw3 --f77flags=-ffixed-line-length-none --fcompiler=gfortran
