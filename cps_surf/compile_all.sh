b1737675eb2e31302d4ba57aea245e0fa0dad8a0
Lili Feng <lili.feng@colorado.edu>
2017-08-28 00:10:42 -0600

    Aug 28th, 2017

--- cps_surf/compile_all.sh
+++ cps_surf/compile_all.sh
@@ -1,7 +1,7 @@
 ####################
 ## copy subs code
 ####################
-cps_subs_dir='/home/leon/code/PROGRAMS.330/SUBS'
+cps_subs_dir='/home/lili/code/PROGRAMS.330/SUBS'
 
 #cp $cps_subs_dir/f2csub.f .
 #cp $cps_subs_dir/f96subf.f .
@@ -14,7 +14,8 @@ cp $cps_subs_dir/mchdep.f .
 cp $cps_subs_dir/lgstr.f .
 #cp $cps_subs_dir/grphsubf.f .
 
-f2py -h tdisp96.pyf -m tdisp96 tdisp96.f mnmarg.f mgtarg.f lgstr.f tio.f mchdep.f  tgetmod.f
-f2py --f77flags="-ffixed-line-length-none -O3" --f90flags="-O3" -c tdisp96.pyf tdisp96.f mnmarg.f mgtarg.f lgstr.f tio.f mchdep.f tgetmod.f
+
+#f2py -h tdisp96.pyf -m tdisp96 tdisp96_subroutine.f mnmarg.f mgtarg.f lgstr.f tio.f mchdep.f  tgetmod.f
+f2py --f77flags="-ffixed-line-length-none -O3" --f90flags="-O3" -c tdisp96.pyf tdisp96_subroutine.f mnmarg.f mgtarg.f lgstr.f tio.f mchdep.f tgetmod.f
 
 
