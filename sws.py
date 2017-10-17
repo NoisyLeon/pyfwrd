5495d7a211b02e9782a4f59e16094a019f125097
Lili Feng <lili.feng@colorado.edu>
2017-10-17 16:34:27 -0600

    Oct 17th, 2017

--- sws.py
+++ sws.py
@@ -51,8 +51,6 @@ class sws_solver(object):
             raise ValueError('Input model should be type of vmodel.model1d !')
         self.model  = inmodel
         self.dt     = 0.025
-        self.delayt = np.array([])
-        self.phi    = np.array([])
         self.bazArr = np.array([])
         ##############################
         # input parameters for raysum
@@ -62,6 +60,12 @@ class sws_solver(object):
         self.align  = 1 # Alignment: 0 is none, 1 aligns on primary phase (P or S)
         self.shift  = 10. # Shift of traces -- t=0 at this time (sec)
         self.outrot = 1  # Rotation to output: 0 is NS/EW/Z, 1 is R/T/Z, 2 is P/SV/SH
+        ############################################################
+        # parameters for shear wave splitting analysis
+        ############################################################
+        self.delayt = np.array([])
+        self.phi    = np.array([])
+        # # self.Crc    = np.array([])
         return
     
     def init_default(self, dh=1., nl=100):
@@ -82,7 +86,7 @@ class sws_solver(object):
         Compute radial and transverse receiver function using raysum
         ===================================================================================================================================
         ::: input parameters :::
-        bazin       - bazimuth array of wave vector
+        bazin       - back-azimuth array of wave vector
         t           - time length of output in sec
         iphase      - initial phase index (1 - P; 2 - SV; 3 - SH)
         slowness    - reference horizontal slowness (default - 0.06 s/km, 1./0.06=16.6667)
@@ -90,7 +94,7 @@ class sws_solver(object):
         ::: output :::
         self.tt     - travel time array (shape - (nphase, ntrace))
         self.amp    - amplitude array (shape - (3, nphase, ntrace))
-        self.trENZ  - ENZ component traces (shape - (3, npts, ntrace))
+        self.trNEZ  - ENZ component traces (shape - (3, npts, ntrace))
         self.trROT  - rotated component traces (shape - (3, npts, ntrace)), will not be available if self.outrot = 0
         ===================================================================================================================================
         """
@@ -155,7 +159,7 @@ class sws_solver(object):
                         self.mults, self.npts, self.dt, self.width, self.align, self.shift, self.outrot, phfname)
         self.tt     = tt[:nphase, :ntr]
         self.amp    = amp[:, :nphase, :ntr]
-        self.trENZ  = tr_cart[:, :self.npts, :ntr]
+        self.trNEZ  = tr_cart[:, :self.npts, :ntr]
         if self.outrot != 0:
             self.trROT  = tr_ph[:, :self.npts, :ntr]
         self.nphase = nphase; self.ntr = ntr
@@ -171,38 +175,135 @@ class sws_solver(object):
         return
     
     def rotate(self, trid, angle, dtype=1):
-        angle   = -angle/180.*np.pi
+        """
+        Rotate the synthetics clockwise by a given angle
+        """
+        angle   = angle/180.*np.pi
         if dtype == 1:
             x       = self.trROT[1, :, trid]
             y       = self.trROT[0, :, trid]
         else:
-            x       = self.trSYN[1, :, trid]
-            y       = self.trSYN[0, :, trid]
-        
+            x       = self.trSYNROT[1, :, trid]
+            y       = self.trSYNROT[0, :, trid]
+        # NOTE: Rotation matrix for a vector counter-clockwise with an angle
+        # It is equivalent to rotate the coordinate system clockwise with the same angle
         compx   = x*np.cos(angle) - y* np.sin(angle)
         compy   = x*np.sin(angle) + y* np.cos(angle)
         return compx, compy
 
-    def convolve(self, fc=2.5):
+    def convolve(self, fc=2.5, rot=True):
         fs          = 1./self.dt
         npts        = min(fs/fc*8, self.npts)
         Nshift      = np.int(1.35/fc/self.dt) - 1
         t0          = Nshift *self.dt
         stf         = RickerIntSignal(dt=self.dt, npts = npts, fc=fc, t0=t0)
-        self.trSYN  = np.zeros(self.trROT.shape) 
+        self.trSYN  = np.zeros(self.trROT.shape)
+        if rot:     self.trSYNROT  = np.zeros(self.trROT.shape)
         for i in xrange(self.ntr):
-            self.trSYN[0, :, i]     = np.convolve(stf, self.trROT[0, :, i])[Nshift:self.npts+Nshift]
-            self.trSYN[1, :, i]     = np.convolve(stf, self.trROT[1, :, i])[Nshift:self.npts+Nshift]
-            self.trSYN[2, :, i]     = np.convolve(stf, self.trROT[2, :, i])[Nshift:self.npts+Nshift]
+            self.trSYN[0, :, i]     = np.convolve(stf, self.trNEZ[0, :, i])[Nshift:self.npts+Nshift]
+            self.trSYN[1, :, i]     = np.convolve(stf, self.trNEZ[1, :, i])[Nshift:self.npts+Nshift]
+            self.trSYN[2, :, i]     = np.convolve(stf, self.trNEZ[2, :, i])[Nshift:self.npts+Nshift]
+            if rot:
+                self.trSYNROT[0, :, i]     = np.convolve(stf, self.trROT[0, :, i])[Nshift:self.npts+Nshift]
+                self.trSYNROT[1, :, i]     = np.convolve(stf, self.trROT[1, :, i])[Nshift:self.npts+Nshift]
+                self.trSYNROT[2, :, i]     = np.convolve(stf, self.trROT[2, :, i])[Nshift:self.npts+Nshift]
         self.stf    = stf
         return
     
-    def rotcorr(self, maxtime = 1.0, twin=3.0, dphi=0.1):
+    def rotcorr(self, trid, maxtime = 1.0, twin=3.0, dphi=.1):
         """
             shear wave splitting using the Rotation-Correlation method
             (e.g. Bowman and Ando,1987)
         """
         maxlags = np.ceil(maxtime/self.dt) # only +-4 seconds relevant
         zerolag = np.ceil(twin/self.dt)
+        phi_test= (np.mgrid[-90:90-dphi:dphi])/180*np.pi
+        ind     = (self.time > (self.shift-twin))*(self.time < (self.shift+twin))
+        Cmatrix = np.zeros([phi_test.size, 2*maxlags+1])
+        SG      = self.trSYNROT[0, :, trid] # R component
+        SH      = self.trSYNROT[1, :, trid] # T component
+        for iphi in xrange(phi_test.size):
+            phi     = -phi_test[iphi]
+            # test slow-fast seismograms
+            xphi    = SH*np.cos(phi) - SG* np.sin(phi)
+            yphi    = SH*np.sin(phi) + SG* np.cos(phi)
+            corTr   = np.correlate(a=xphi[ind], v=yphi[ind], mode='full')/(np.linalg.norm(xphi[ind])*np.linalg.norm(yphi[ind]))
+            L       = int((corTr.size - 1)/2)
+            Cmatrix[iphi, :] = corTr[L-maxlags:L+maxlags+1]
+        indmax  = Cmatrix.argmax()
+        pmax    = np.floor(indmax/(2*maxlags+1))
+        tmax    = indmax - pmax*(2*maxlags+1)
+        delayt  = (tmax-maxlags)*self.dt
+        fa_phi  = phi_test[pmax]/np.pi*180.
+        # if delayt < 0.:
+        #     print 'neg delay time'
+        #     delayt  = -delayt
+        #     fa_phi  += 90.
+        #     if fa_phi > 90.:
+        #         fa_phi -= 180.
+        return delayt, fa_phi, Cmatrix
+    
+    def rotcorr_st(self, maxtime = 1.0, twin=3.0, dphi=.1):
+        """
+            shear wave splitting using the Rotation-Correlation method for the stream (all the traces)
+        """
+        Crc         = []
+        for trid in xrange(self.ntr):
+            delayt, fa_phi, Cmatrix = self.rotcorr(trid=trid, maxtime = maxtime, twin=twin, dphi=dphi)
+            self.delayt = np.append(self.delayt, delayt)
+            self.phi    = np.append(self.phi, fa_phi)
+            Crc.append(Cmatrix)
+        # self.fa_phi = self.bazArr + 180. 
+        self.Crc    = np.array(Crc)
+        return
+    
+    def silverchan(self, trid, maxtime = 1.0, twin=3.0, dphi=.1):
+        """
+            shear wave splitting using the Silver & Chan method (1991)
+        """
+        maxlags = np.ceil(maxtime/self.dt) # only +-4 seconds relevant
+        zerolag = np.ceil(twin/self.dt)
+        phi_test= (np.mgrid[-90:90-dphi:dphi])/180*np.pi
+        dt_test = np.mgrid[0:maxtime:self.dt]
+        ind     = np.where((self.time > (self.shift-twin))*(self.time < (self.shift+twin)))[0]
+        SG      = self.trSYNROT[0, :, trid] # N component
+        SH      = self.trSYNROT[1, :, trid] # E component
+        Ematrix = np.zeros([phi_test.size, dt_test.size])
+        l1      = np.zeros([phi_test.size, dt_test.size])
+        l2      = np.zeros([phi_test.size, dt_test.size])
+        for iphi in xrange(phi_test.size):
+            phi     = -phi_test[iphi]
+            xphi    = SH*np.cos(phi) - SG* np.sin(phi)
+            yphi    = SH*np.sin(phi) + SG* np.cos(phi)
+            tmpfast = xphi[ind]
+            for idt  in xrange(dt_test.size):
+                dt      = dt_test[idt]
+                tmpslow = yphi[ind+idt]
+                SHc     = tmpfast*np.cos(-phi) - tmpslow* np.sin(-phi)
+                SGc     = tmpfast*np.sin(-phi) + tmpslow* np.cos(-phi)
+                Ematrix[iphi, idt] = np.linalg.norm(SHc) # Energy on transverse component
+                SHe     = SHc - SHc.mean()
+                SGe     = SGc - SGc.mean()
+                # construct covariance matrix
+                covar   = np.cov(SHe, SGe)
+                # eigenvalue estimation
+                w, v    = np.linalg.eig(covar)
+                # print w
+                # print v
+                # return
+                l1[iphi, idt]   = w[0]
+                l2[iphi, idt]   = w[1]
+        indmin  = Ematrix.argmin()
+        pmin    = np.floor(indmin/(dt_test.size))
+        tmin    = indmin - pmin*(dt_test.size)
+        
+        delayt  = dt_test[tmin]
+        fa_phi  = phi_test[pmin]/np.pi*180.
+        
+        # print delayt, fa_phi
+        # return Ematrix, l1, l2
+                
+                
+        
     
     
